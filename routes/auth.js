39e67496c2c449b03df69557aa748e28eeca2039
guja kupreishvili <gujakupreishvili4@gmail.com>
2025-11-15 20:45:25 +0400

    feat:gettAllUser model controller routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { register ,getUserById , login} = require("../controllers/authController");
+const { register ,getUserById , login ,getUsers} = require("../controllers/authController");
 
 router.post("/register", register);
 router.get("/getUserById/:id", getUserById )
 router.post("/login", login);
+router.get("/getAllUsers", getUsers);
 
 module.exports = router;
